fb1ff0af7cdbc804dd4a1d480b444d361cee23c6
Laerifer <127577092+Laerifer@users.noreply.github.com>
2024-08-07 08:14:17 -0600

    Actualizar app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def serve_static(path=''):
     return send_from_directory(os.getcwd(), path) if path else send_from_directory(os.getcwd(), 'index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
