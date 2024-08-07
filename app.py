345c4404056dee1c2bb67963e1ce1d0e99227724
Laerifer <127577092+Laerifer@users.noreply.github.com>
2024-08-07 07:36:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<path:path>')
 def serve_static(path=''):
-    # Define la carpeta raíz donde se encuentran los archivos estáticos
-    root_dir = os.path.join(os.getcwd(), 'Main')
-    
-    # Sirve archivos desde la carpeta 'Main'
-    return send_from_directory(root_dir, path) if path else send_from_directory(root_dir, 'index.html')
+    # Sirve archivos desde el directorio actual
+    return send_from_directory(os.getcwd(), path) if path else send_from_directory(os.getcwd(), 'index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
