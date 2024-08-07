24676688f1f87e48b28d5676e829b977e6533e96
Laerifer <127577092+Laerifer@users.noreply.github.com>
2024-08-07 00:26:02 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+from flask import Flask, send_from_directory
+import os
 
-port = 8080  # Debe coincidir con el puerto configurado en App Runner y expuesto en Docker
+app = Flask(__name__)
 
-class CustomHandler(SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
-
-def run(server_class=HTTPServer, handler_class=CustomHandler):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Servidor corriendo en http://0.0.0.0:{port}')
-    httpd.serve_forever()
+@app.route('/')
+@app.route('/<path:path>')
+def serve_static(path=''):
+    # Define la carpeta raíz donde se encuentran los archivos estáticos
+    root_dir = os.path.join(os.getcwd(), 'Main')
+    
+    # Sirve archivos desde la carpeta 'Main'
+    return send_from_directory(root_dir, path) if path else send_from_directory(root_dir, 'index.html')
 
 if __name__ == "__main__":
-    run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
